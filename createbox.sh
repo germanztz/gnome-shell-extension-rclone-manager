8f1b000c792b996d58554d5d25f1af158a13d3d6
German Z <germanztz@avena.tk>
2023-06-06 14:32:38 +0200

    create vm

--- createbox.sh
+++ createbox.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Name of the new virtual machine
-vm_name="Ubuntu22.04"
+vm_name="Ubuntu23.04"
 
 # Location of the ISO file
-iso_file=$HOME"/Descargas/ubuntu-22.04.1-desktop-amd64.iso"
+iso_file=$HOME"/Descargas/ubuntu-23.04-desktop-amd64.iso"
 
 # Location to store virtual machine files
 vm_folder=$HOME"/VirtualBox VMs"
@@ -13,25 +13,25 @@ vm_folder=$HOME"/VirtualBox VMs"
 VBoxManage createvm --name $vm_name --register --basefolder "$vm_folder"
 
 # Set virtual machine type and version
-VBoxManage modifyvm $vm_name --ostype "Ubuntu_64" --memory 2048 --vram 128
+# Configure video driver to vmsvga
+# Create a network interface for the virtual machine
+VBoxManage modifyvm $vm_name --ostype "Ubuntu_64" --memory 4096 --vram 128 \
+ --vram 128 --accelerate3d on --graphicscontroller vmsvga \
+ --nic1 nat
 
 # Add a SATA controller for the virtual hard drive
 VBoxManage storagectl $vm_name --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storagectl $vm_name --name "IDE Controller" --add ide
 
 # Add a virtual hard drive
 VBoxManage createhd --filename "$vm_folder"/$vm_name/$vm_name.vdi --size 40960 --format VDI
 VBoxManage storageattach $vm_name --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$vm_folder"/$vm_name/$vm_name.vdi
-
-# Add a virtual optical drive and attach the ISO file
-VBoxManage storagectl $vm_name --name "IDE Controller" --add ide
 VBoxManage storageattach $vm_name --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium $iso_file
 
-# Create a network interface for the virtual machine
-VBoxManage modifyvm $vm_name --nic1 nat
 
 # Set up an unattended installation
 # VBoxManage unattended install $vm_name --user=vagrant --password=vagrant \
 # --locale=en_US --country=US --time-zone=UTC --hostname=$vm_name --iso=$iso_file
 
 # Start the virtual machine
-# VBoxManage startvm $vm_name
+VBoxManage startvm $vm_name
